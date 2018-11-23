a7468a66422cf31735338a87573ea818eb46dd29
SerekKiri <kacper.potyrala@gmail.com>
2018-11-23 20:47:42 +0100

    assets/: Adds images and icons

---
+++ lib/travy.glyphs.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class TravyGlyphs {
+  TravyGlyphs._();
+
+  static const _kFontFam = 'TravyGlyphs';
+
+  static const IconData github_circled = const IconData(0xf09b, fontFamily: _kFontFam);
+  static const IconData github = const IconData(0xf113, fontFamily: _kFontFam);
+  static const IconData github_squared = const IconData(0xf300, fontFamily: _kFontFam);
+}
