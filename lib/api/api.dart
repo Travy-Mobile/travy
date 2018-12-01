af16e0c2e7e889c216c12847da9f2a874566a056
NNeast <zawislaktomasz4@gmail.com>
2018-12-01 21:13:52 +0100

    feat: add basic func to fetch builds

---
+++ lib/api/api.dart
@@ -0,0 +1,22 @@
+import 'package:http/http.dart';
+import 'package:travy/models/build.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Api {
+  
+  Future<List<Build>> fetchBuilds() async{
+    String url = "https://api.travis-ci.org/builds";
+    
+    var response = await http.get(url, headers: {
+      "Authorization": "token xxx",
+      "Travis-API-Version": "3"
+    });
+
+    var decoded = json.decode(response.body);
+  }
+
+}
+
+final api = Api();
