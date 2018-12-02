c7e4a2fa0bf55a5600d66790c719ece0f1965433
NNeast <zawislaktomasz4@gmail.com>
2018-12-02 17:56:04 +0100

    feat: add simple home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Container(
+         
+       ),
+    );
+  }
+}
