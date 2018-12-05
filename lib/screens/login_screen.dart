2736fc7f7f29218d3dee4690833e477480b95252
NNeast <zawislaktomasz4@gmail.com>
2018-12-05 23:20:39 +0100

    :ambulance: fix: move Scaffold to screens

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,59 +3,31 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.only(top: 100.0),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(right: 20),
-                child: Container(
-                  width: 170,
-                  height: 170,
-                  child: Image.asset("logo.png"),
-                )   
+    return Scaffold(
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.only(top: 100.0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 20),
+                  child: Container(
+                    width: 170,
+                    height: 170,
+                    child: Image.asset("logo.png"),
+                  )   
+                ),
+                Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 30.0),
+                child: Text("Travy",
+                style: TextStyle(
+                  fontSize: 45,
+                  fontFamily: "ReemKufi",
+                  color: Color.fromRGBO(102, 102, 102, 10)
+                ),),
               ),
-              Padding(
-              padding: EdgeInsets.only(top: 5.0, bottom: 30.0),
-              child: Text("Travy",
-              style: TextStyle(
-                fontSize: 45,
-                fontFamily: "ReemKufi",
-                color: Color.fromRGBO(102, 102, 102, 10)
-              ),),
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: Card(
-                margin: EdgeInsets.only(left: 10, right: 10),
-                child: Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Row(
-                    children: <Widget>[
-                      Image.asset("logo.png", height: 80, width: 80,),
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.only(right: 30),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Text("Travis CI (.com)"),
-                              Text("For open source projects")
-                            ],
-                          ),
-                        )
-                      )
-                    ],
-                  ),
-                )  
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 5),
-              child: GestureDetector(
+              GestureDetector(
                 onTap: () {},
                 child: Card(
                   margin: EdgeInsets.only(left: 10, right: 10),
@@ -63,7 +35,7 @@ class LoginScreen extends StatelessWidget {
                     padding: EdgeInsets.all(5),
                     child: Row(
                       children: <Widget>[
-                        Image.asset("logo2.png", height: 80, width: 80,),
+                        Image.asset("logo.png", height: 80, width: 80,),
                         Expanded(
                           child: Padding(
                             padding: EdgeInsets.only(right: 30),
@@ -72,7 +44,7 @@ class LoginScreen extends StatelessWidget {
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: <Widget>[
                                 Text("Travis CI (.com)"),
-                                Text("For private projects")
+                                Text("For open source projects")
                               ],
                             ),
                           )
@@ -81,10 +53,40 @@ class LoginScreen extends StatelessWidget {
                     ),
                   )  
                 ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Card(
+                    margin: EdgeInsets.only(left: 10, right: 10),
+                    child: Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Row(
+                        children: <Widget>[
+                          Image.asset("logo2.png", height: 80, width: 80,),
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 30),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  Text("Travis CI (.com)"),
+                                  Text("For private projects")
+                                ],
+                              ),
+                            )
+                          )
+                        ],
+                      ),
+                    )  
+                  ),
+                )
               )
-            )
-            ],
-          ),
+              ],
+            ),
+          )
         )
       )
     );
